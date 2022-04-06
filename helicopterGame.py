c9ee506d0c74b3f2a43a1f982c44ec1211f3b325
Hadi Aghazadeh <hadia667@gmail.com>
2022-04-06 23:37:57 +0430

    handle text for gameover

--- helicopterGame.py
+++ helicopterGame.py
@@ -1,13 +1,14 @@
 
 import pygame
+import time
 
 black = (0,0,0)
 white = (255,255,255)
-max_x = 800
-max_y = 400
+surfaceWidth = 800
+surfaceHeight = 500
 
 pygame.init()
-surface = pygame.display.set_mode((max_x,max_y))
+surface = pygame.display.set_mode((surfaceWidth,surfaceHeight))
 pygame.display.set_caption('Helicopter')
 clock = pygame.time.Clock()
 
@@ -15,9 +16,22 @@ img = pygame.image.load('Helicopter.png')
 x = 150
 y = 200
 
+def msgSurface(text):
+    smallText = pygame.font.Font('freesansbold.ttf',20)
+    largeText = pygame.font.Font('freesansbold.ttf',150)
+
+    titleTextSurf, titleTextRect = makeTextObjs(text,largeText)
+    titleTextRect.center = surfaceWidth / 2 , surfaceHeight / 2
+    surface.blit(titleTextSurf,titleTextRect)
+
+    typTextSurf, typTextRect = makeTextObjs('Press any key to continue!', smallText)
+    typTextRect.center = surfaceWidth / 2 , ((surfaceHeight / 2)+100)
+    surface.blit(typTextSurf, typTextRect)
+    pygame.display.update()
+    time.sleep(1)
+
 def gameOver():
-    pygame.quit()
-    quit()
+    msgSurface('Kaboom!')
 
 def helicopter(x,y,image):
     surface.blit(image,(x,y))
@@ -44,7 +58,7 @@ while not game_over:
     surface.fill(black)
     helicopter(x,y,img)
 
-    if y > (max_y - 40) or y <0:
+    if y > (surfaceHeight - 40) or y <0:
         gameOver()
 
     pygame.display.update()
