0be6e21d2d7e7974a4fbc6e112ef68039f751a45
Hadi Aghazadeh <hadia667@gmail.com>
2022-05-12 16:29:05 +0430

    Add deep Q model

--- snakeRL/agent.py
+++ snakeRL/agent.py
@@ -5,7 +5,7 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
-
+from model import linear_QNet, QTrainer
 #Parameters
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -16,10 +16,10 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 #randomness
-        self.gamma = 0 #discount rate
+        self.gamma = 0.9 #discount rate
         self.memory = deque(maxlen = MAX_MEMORY)    #popleft
-        self.model = None
-        self.trainer = None
+        self.model = linear_QNet(11,256,3)
+        self.trainer = QTrainer(self.model, lr= LR, gamma= self.gamma)
 
 
     def get_state(self, game):
@@ -92,7 +92,7 @@ class Agent:
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype = torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
         return final_move
@@ -129,7 +129,7 @@ def train():
 
             if score > record:
                 record = score
-                #agent.model.save()
+                agent.model.save()
 
             print('Game', agent.n_games, 'Score', score, 'Record', record)
 
--- snakeRL/model.py
+++ snakeRL/model.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -11,4 +12,25 @@ class linear_QNet(nn.Module):
         self.linear2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        
+        x = F.relu(self.linear1(x))
+        x = self.linear2
+        return x
+
+    def save(self, file_name = 'model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr = self.lr)
+        self.criterion = nn.MSELoss
+
+    def train_step(self, state, action, reward, next_state, done):
+        pass
