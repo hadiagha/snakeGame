0b79b64f5b79eb95d5c44a903c106e78a8fc2fb9
Hadi Aghazadeh <hadia667@gmail.com>
2022-05-12 17:02:33 +0430

    add plot function

--- snakeRL/agent.py
+++ snakeRL/agent.py
@@ -1,11 +1,14 @@
 
 #imprt libraries
+from statistics import mean
 import torch
 import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
 from model import linear_QNet, QTrainer
+from helper import plot
+
 #Parameters
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -133,7 +136,12 @@ def train():
 
             print('Game', agent.n_games, 'Score', score, 'Record', record)
 
-            #To DO: plot 56'
+            #plot the results
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 
 
