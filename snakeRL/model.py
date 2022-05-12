18e8d78250f2a78b9b5d0ed836df227c0e51bb04
Hadi Aghazadeh <hadia667@gmail.com>
2022-05-12 16:51:56 +0430

    Complete RL model

--- snakeRL/model.py
+++ snakeRL/model.py
@@ -33,4 +33,33 @@ class QTrainer:
         self.criterion = nn.MSELoss
 
     def train_step(self, state, action, reward, next_state, done):
-        pass
+        state = torch.tensor(state, dtype= torch.float)
+        next_state = torch.tensor(next_state, dtype= torch.float)
+        action = torch.tensor(action, dtype= torch.float)
+        reward = torch.tensor(reward, dtype= torch.float)
+
+        if len(state.shape) ==1:
+            # (1,x)
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        # 1: predicted Q values with current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.moel(next_state[idx]))
+
+            target[idx][torch.argmax(action).item()] = Q_new
+        # 2: Q_new = r + y * max( next predicted Q value) -> only do this id not done
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
