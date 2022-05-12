2b159a839244baab82bebe427ef6774859a500ba
Hadi Aghazadeh <hadia667@gmail.com>
2022-05-12 18:38:34 +0430

    debugging

--- snakeRL/helper.py
+++ snakeRL/helper.py
@@ -4,7 +4,7 @@ from IPython import display
 plt.ion()
 
 def plot(scores, mean_scores):
-    display.clear_output(wait = True)
+    display.clear_output(wait=True)
     display.display(plt.gcf())
     plt.clf()
     plt.title('Training...')
@@ -12,6 +12,8 @@ def plot(scores, mean_scores):
     plt.ylabel('Score')
     plt.plot(scores)
     plt.plot(mean_scores)
-    plt.ylim(ymin = 0)
-    plt.text(len(scores)-1 , scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores)-1 , mean_scores[-1], str(mean_scores[-1]))
+    plt.ylim(ymin=0)
+    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.show(block=False)
+    plt.pause(.1)
