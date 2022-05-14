64a4322bfe690b9fda8aa07bec94fc70f5dae012
Hadi Aghazadeh <hadia667@gmail.com>
2022-05-14 14:44:48 +0430

    organizae more

--- snakeRL/agent.py
+++ snakeRL/agent.py
@@ -60,7 +60,7 @@ class Agent:
             dir_r,
             dir_u,
             dir_d,
-            
+
             # Food location 
             game.food.x < game.head.x,  # food left
             game.food.x > game.head.x,  # food right
@@ -81,8 +81,6 @@ class Agent:
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
-        #for state, action, reward, nexrt_state, done in mini_sample:
-        #    self.trainer.train_step(state, action, reward, next_state, done)
 
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
